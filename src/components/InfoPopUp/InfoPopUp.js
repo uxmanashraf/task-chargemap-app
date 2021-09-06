1424763a45594338488fffec4bddd7cfe20df4cf
usman <ashrafusman93@gmail.com>
2021-09-06 09:55:33 +0200

    Info Window to show details on Map Card

--- src/components/InfoPopUp/InfoPopUp.js
+++ src/components/InfoPopUp/InfoPopUp.js
@@ -1,16 +1,99 @@
-import React from 'react';
-import { Card } from 'antd';
+import React from "react";
+import { Skeleton, Card, Table, Row, Col } from "antd";
+
+const style = { background: "#0092ff", padding: "8px 0" };
 
 const InfoPopUp = (props) => {
-    return (
-        <Card  style={{ width: 300 }}>
-            <h3>{props.chargePoint.address.street} {props.chargePoint.address.zip} {props.chargePoint.address.town} ({props.chargePoint.address.countryCode})</h3>
-            <p><strong>Latitude</strong> {props.chargePoint.location.lat}</p>
-            <p><strong>Longitude</strong> {props.chargePoint.location.lon}</p>
-            <p><strong>Pricing Info</strong> {props.chargePoint.priceInfo}</p>
-            <p><strong>Available</strong> {props.chargePoint.available? "Yes": "No"}</p>
-        </Card>
-    );
+  const columns = [
+    {
+      title: "#",
+      dataIndex: "key",
+      key: "key",
+    },
+    {
+      title: "plug type",
+      dataIndex: "type",
+      key: "type",
+    },
+    {
+      title: "max power",
+      dataIndex: "maxPowerInKW",
+      key: "maxPowerInKW",
+    },
+  ];
+
+  const connectors = props.chargePoint.connectors.map((connector, index) => {
+    return {
+      key: index,
+      type: connector.type,
+      maxPowerInKW: connector.maxPowerInKW,
+    };
+  });
+
+  const distanceElem = props.chargePoint.calculatedDistance ? (
+    (props.chargePoint.calculatedDistance/1000).toFixed(1) + " KM"
+  ) : (
+    <Skeleton.Button active={true} size="small" shape="round" block={false} />
+  );
+
+  return (
+    <Card bordered={false} style={{ width: 450 }}>
+      <Row>
+        <Col span={24}>
+          <h4>
+            {props.chargePoint.address.street} {props.chargePoint.address.zip}{" "}
+            {props.chargePoint.address.town} (
+            {props.chargePoint.address.countryCode})
+          </h4>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col className="gutter-row" span={12}>
+          <strong>Operator</strong>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <strong>Latitude</strong>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          {props.chargePoint.location.lat}
+        </Col>
+
+        <Col className="gutter-row" span={12}>
+          {props.chargePoint.operatorName}
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <strong>Longitude</strong>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          {props.chargePoint.location.lon}
+        </Col>
+
+        <Col className="gutter-row" span={12}></Col>
+
+        <Col className="gutter-row" span={6}>
+          <strong>Distance</strong>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          {distanceElem}
+        </Col>
+
+        <Col className="gutter-row" span={18}>
+          <Table
+            bordered
+            size="small"
+            pagination={false}
+            columns={columns}
+            dataSource={connectors}
+          />
+        </Col>
+        <Col className="gutter-row" span={6}></Col>
+      </Row>
+
+      <p>
+        <strong>Available</strong> {props.chargePoint.available ? "Yes" : "No"}
+      </p>
+    </Card>
+  );
 };
 
 export default InfoPopUp;
